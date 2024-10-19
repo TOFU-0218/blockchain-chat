f080979ba5da9604ab6a16036e89c9e68e0a2376
TOFU-0218 <maikura5963@gmail.com>
2024-10-19 16:25:32 +0000

    test: 変数関連まとめた

--- tests/test_fileIO.py
+++ tests/test_fileIO.py
@@ -5,23 +5,25 @@ from blockchain_chat.fileIO import load_data, init_data
 # データロードテスト
 def test_load_data(tmp_path):
     # テスト用のJSONデータ
-    test_data = {
-        "0":{
-            "from": "Alice",
-            "to": "Bob",
-            "message": "Hello!",
-            "time": "2020-01-01 12:00:00",
-            "hash": "4c716d4cf211c7b7d2f3233c941771ad0507ea5bacf93b492766aa41ae9f720d"
-        },
-        "1":{
-            "from": "Bob",
-            "to": "Alice",
-            "message": "Hi!",
-            "time": "2020-01-01 12:01:00",
-            "hash": "daca2e083e456dff21ff10998c1e99f414e45e5f3f0709af0b33b86df4694d19"
-        }
+test_data = {
+    "0":{
+        "from": "Alice",
+        "to": "Bob",
+        "message": "Hello!",
+        "time": "2020-01-01 12:00:00",
+        "hash": "4c716d4cf211c7b7d2f3233c941771ad0507ea5bacf93b492766aa41ae9f720d"
+    },
+    "1":{
+        "from": "Bob",
+        "to": "Alice",
+        "message": "Hi!",
+        "time": "2020-01-01 12:01:00",
+        "hash": "daca2e083e456dff21ff10998c1e99f414e45e5f3f0709af0b33b86df4694d19"
     }
+}
 
+# データロードテスト
+def test_load_data(tmp_path):
     # 一時的なJSONファイルを作成
     json_file = tmp_path / "test_data.json"
     with json_file.open('w', encoding = 'utf-8') as f:
