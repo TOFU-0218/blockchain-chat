ca9ff69b8c8165b144bf3c8267154cc28b7bcef0
TOFU-0218 <maikura5963@gmail.com>
2024-10-21 14:10:42 +0000

    add: ブロック追加機能追加 #2

--- blockchain_chat/blockchain.py
+++ blockchain_chat/blockchain.py
@@ -1,5 +1,17 @@
+import time
+
 from blockchain_chat.block import Block
 
 class Blockchain:
     def __init__(self):
-        self.chain = []
+        self.chain = {}
+
+    def add_block(self, sender, recipient, message):
+        if len(self.chain) == 0:
+            previous_hash = "genesis"
+        else:
+            previous_hash = list(self.chain.values())[-1].hash
+        index = len(self.chain)
+        timestamp = time.time()
+        block = Block(sender, recipient, message, timestamp, previous_hash, index)
+        self.chain[index] = block
--- tests/test_blockchain.py
+++ tests/test_blockchain.py
@@ -3,4 +3,12 @@ from blockchain_chat.blockchain import Blockchain
 # ブロックチェーン初期化テスト
 def test_create_blockchain():
     blockchain = Blockchain()
-    assert blockchain.chain == []
+    assert blockchain.chain == {}
+
+# ブロック追加テスト
+def test_add_block():
+    blockchain = Blockchain()
+    blockchain.add_block("Alice", "Bob", "Hello!")
+    blockchain.add_block("Bob", "Alice", "Hi!")
+    assert len(blockchain.chain) == 2
+    print(blockchain.chain)
